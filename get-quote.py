65114b72828d6c42b926e05ddcf49ae069c99744
mahen2-cmd <67423345+mahen2-cmd@users.noreply.github.com>
2021-01-02 15:28:03 +0500

    Github tutorial done

--- get-quote.py
+++ get-quote.py
@@ -7,8 +7,20 @@ def primary():
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1 
-  rnd = random.randint(0,last)
-  print(quotes[rnd])
+  rnd1 = random.randint(0,last)
+  rnd2 = random.randint(0,last)
+  str1 = quotes[rnd1].rstrip('\n')
+  str2 = quotes[rnd2].rstrip('\n')
+
+  
+  print(str1,str2)
+
+  str3 = input()
+  str3 = str3 + '\n'
+  
+  
+  with open("quotes.txt", "a") as a_writer:
+    a_writer.write(str3)
 
 if __name__== "__main__":
   primary()
